ec02c864303f01ca550b67ec84fe6d0547267974
Mateusz Stopiński <mateusz.stopinski@pushpushgo.com>
2025-09-17 10:51:32 +0200

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# @pushpushgo/react-native-sdk
+
+a
+
+## Installation
+
+
+```sh
+npm install @pushpushgo/react-native-sdk
+```
+
+
+## Usage
+
+
+```js
+import { multiply } from '@pushpushgo/react-native-sdk';
+
+// ...
+
+const result = multiply(3, 7);
+```
+
+
+## Contributing
+
+- [Development workflow](CONTRIBUTING.md#development-workflow)
+- [Sending a pull request](CONTRIBUTING.md#sending-a-pull-request)
+- [Code of conduct](CODE_OF_CONDUCT.md)
+
+## License
+
+MIT
+
+---
+
+Made with [create-react-native-library](https://github.com/callstack/react-native-builder-bob)
---
+++ android/src/main/java/com/pushpushgo/reactnativesdk/PushPushGoPackage.kt
@@ -0,0 +1,19 @@
+package com.pushpushgo.reactnativesdk
+
+import com.facebook.react.BaseReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.module.model.ReactModuleInfo
+import com.facebook.react.module.model.ReactModuleInfoProvider
+
+class PushPushGoPackage : BaseReactPackage() {
+  override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? {
+    return null
+  }
+
+  override fun getReactModuleInfoProvider(): ReactModuleInfoProvider {
+    return ReactModuleInfoProvider {
+      emptyMap()
+    }
+  }
+}
