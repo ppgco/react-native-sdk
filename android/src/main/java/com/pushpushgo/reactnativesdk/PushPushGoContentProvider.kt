79b3f9badb2281d10205eac7f52427a2487ba1b0
Mateusz Stopiński <mateusz.stopinski@pushpushgo.com>
2025-09-22 16:34:59 +0200

    feat(android): improve setup in manifest

--- android/src/main/java/com/pushpushgo/reactnativesdk/PushPushGoContentProvider.kt
+++ android/src/main/java/com/pushpushgo/reactnativesdk/PushPushGoContentProvider.kt
@@ -3,17 +3,34 @@ package com.pushpushgo.reactnativesdk
 import android.app.Application
 import android.content.ContentProvider
 import android.content.ContentValues
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.database.Cursor
 import android.net.Uri
+import com.pushpushgo.reactnativesdk.bridge.PushPushGoError
 import com.pushpushgo.sdk.PushPushGo
 
 class PushPushGoContentProvider: ContentProvider() {
   override fun onCreate(): Boolean {
     context?.applicationContext?.let { ctx ->
       val app = ctx as Application
+      val metadata = ctx
+          .packageManager
+          .getApplicationInfo(ctx.packageName, PackageManager.GET_META_DATA)
+          .metaData
 
       if (!PushPushGo.isInitialized()) {
-        PushPushGo.getInstance(app)
+        PushPushGo.getInstance(
+          application = app,
+          projectId = metadata.getString("com.pushpushgo.projectId") ?: throw PushPushGoError("com.pushpushgo.projectId is required"),
+          apiKey = metadata.getString("com.pushpushgo.apiKey") ?: throw PushPushGoError("com.pushpushgo.apiKey is required"),
+          isDebug = metadata.getBoolean("com.pushpushgo.isDebug"),
+          isProduction = metadata.getBoolean("com.pushpushgo.isProduction")
+        )
+
+        PushPushGo
+          .getInstance()
+          .setCustomClickIntentFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
       }
 
       app.registerActivityLifecycleCallbacks(PushPushGoActivityCallbacks())
